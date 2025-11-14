783553e52ed51ae11901ebeec1e0d4abe85bbcbd
Ishan Roy <ishanroy3118107@gmail.com>
2025-11-15 01:49:30 +0530

    First Commit

---
+++ backend/main.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+load_dotenv()
+
+CLIENT_URL = os.getenv("CLIENT_URL") or "http://localhost:5173"
+PORT = int(os.getenv("PORT")) or 8000
+
+
+class ServerStatus(BaseModel):
+    status: str
+    message: str
+
+
+app = FastAPI()
+
+origins = [CLIENT_URL]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", response_model=ServerStatus, status_code=200)
+def server_live():
+    return ServerStatus(status="Success", message="Server is live!")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=PORT)
